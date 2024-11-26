6ab06fcd28b7d9fa93638b54c274ab9ee18ebbfb
itallume <itallo.oliver21@gmail.com>
2024-11-26 03:00:19 -0300

    requisição na API

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const urlInputElement = document.getElementById("urlInput");
 let activeDuration = "1";
 const durationElements = {
     "1": document.getElementById("one"),
@@ -14,4 +15,26 @@ function changeDuration(duration) {
     activeDuration = duration;
     durationElements[duration].classList.remove("unselected");
     durationElements[duration].classList.add("selected");
+}
+
+async function generateShortenerUrl() {
+    const originalURL = urlInputElement.value;
+    const date = new Date();
+    date.setDate(date.getDate() + Number(activeDuration));
+    const expirationTimeInSeconds = Math.floor(date.getTime() / 1000).toString();
+    const urlData = {
+        "originalUrl": originalURL,
+        "expirationTime": expirationTimeInSeconds
+    }
+    getCodeUrl(urlData)
+    .then(response => alert(`A sua URL encurtada: https://sjx4g5l0ag.execute-api.us-east-2.amazonaws.com/${response["code"]}`))
+}
+
+const getCodeUrl = async (urlData) => {
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(urlData)
+    }
+    return await fetch('https://sjx4g5l0ag.execute-api.us-east-2.amazonaws.com/create', options)
+    .then(r => r.json());
 }
